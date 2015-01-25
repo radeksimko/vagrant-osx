036836c4391741db8638c26cd58986e9b41e517e
Radek Simko <radek.simko@gmail.com>
2015-01-25 19:28:54 +0000

    Prevent creating keychain if it exists

--- setup-keychain.sh
+++ setup-keychain.sh
@@ -1,3 +1,6 @@
-security create-keychain -p vagrant default
+if [[ `security list-keychains | wc -l` -eq 1 ]]; then
+  security create-keychain -p vagrant default
+fi
+
 security default-keychain -d user -s default
 security unlock-keychain -p vagrant
