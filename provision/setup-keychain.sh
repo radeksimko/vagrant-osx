5579e935661cc96416521bc01f87afe4eaf79125
Radek Simko <radek.simko@gmail.com>
2015-02-15 20:36:21 +0000

    Prevent keychain from locking

--- provision/setup-keychain.sh
+++ provision/setup-keychain.sh
@@ -3,6 +3,9 @@ if [[ `security list-keychains | grep default | wc -l` -eq 0 ]]; then
 
   security create-keychain -p vagrant default
   security default-keychain -d user -s default
+
+  # Overriding default settings (lock-on-sleep timeout=300s)
+  security set-keychain-settings default
 fi
 
 security unlock-keychain -p vagrant
