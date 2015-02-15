6f9e4b7a644231526291dc1d7e1f027a18395292
Radek Simko <radek.simko@gmail.com>
2015-02-15 20:34:45 +0000

    Only set default keychain if not set

--- provision/setup-keychain.sh
+++ provision/setup-keychain.sh
@@ -2,7 +2,7 @@ echo "Setting up keychain..."
 
 if [[ `security list-keychains | grep default | wc -l` -eq 0 ]]; then
   security create-keychain -p vagrant default
+  security default-keychain -d user -s default
 fi
 
-security default-keychain -d user -s default
 security unlock-keychain -p vagrant
