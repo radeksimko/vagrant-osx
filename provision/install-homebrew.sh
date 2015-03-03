ed5beb9d46481d463089cb2fb96771cb44c9222c
Radek Simko <radek.simko@gmail.com>
2015-03-03 08:02:23 +0000

    Brewcask installation + dev mode added

--- provision/install-homebrew.sh
+++ provision/install-homebrew.sh
@@ -5,3 +5,19 @@ if [ ! -d /usr/local/.git ]; then
 else
   echo "Homebrew is already installed"
 fi
+
+cd /vagrant/homebrew-cask
+
+if [ ! -d /usr/local/Library/Taps/caskroom/homebrew-cask ]; then
+  echo "Installing Brewcask ..."
+  brew install caskroom/cask/brew-cask
+else
+  echo "Brewcask is already installed"
+fi
+
+if [[ -e "./production_rubylib" ]]; then
+  echo "Brewcask is already in DEV mode"
+else
+  echo "Turning Homebrew Cask into DEV mode ..."
+  ./developer/bin/develop_brew_cask
+fi
